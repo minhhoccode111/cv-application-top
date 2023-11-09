8da23eaca16aa9b55e90038738a4543f3e0987a2
Đặng Hoàng Minh <minhhoccode111@gmail.com>
2023-11-09 22:25:42 +0700

    Init React project with Bun, Pre-installed tailwindcss, uuid and setup environment

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+// import { useState } from 'react';
+// import { v4 as uuid } from 'uuid';
+import './App.css';
+
+export const App = () => {
+  return (
+    <>
+      <h1 className="text-4xl text-center bg-cyan-600 text-white p-8">Hello, World!</h1>
+    </>
+  );
+};
