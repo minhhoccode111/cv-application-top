b817c0f9be739449cf4fa15bebe2555b67a9c4c6
Đặng Hoàng Minh <minhhoccode111@gmail.com>
2023-11-10 18:31:50 +0700

    Gathering all assets this project will need

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,32 @@
-// import { useState } from 'react';
-// import { v4 as uuid } from 'uuid';
+// eslint-disable-next-line no-unused-vars
+import { useState } from 'react';
+// eslint-disable-next-line no-unused-vars
+import { v4 as uuid } from 'uuid';
+import { Content, Customize, Delete, Education, Experience, Email, Phone, Location, CVImage, Show, Hide, Add, Up, Down, Personal, Download, Github } from './components/Icons';
 import './App.css';
+
+let accent = '#f00';
 
 export const App = () => {
   return (
     <>
-      <h1 className="text-4xl text-center bg-cyan-600 text-white p-8">Hello, World!</h1>
+      <CVImage width="100px" styles={{ borderRadius: '100%' }} />
+      <Content color={accent} />
+      <Customize color={accent} />
+      <Delete color={accent} />
+      <Education color={accent} />
+      <Experience color={accent} />
+      <Email color={accent} />
+      <Phone color={accent} />
+      <Location color={accent} />
+      <Show color={accent} />
+      <Hide color={accent} />
+      <Add color={accent} />
+      <Up color={accent} />
+      <Down color={accent} />
+      <Personal color={accent} />
+      <Download color={accent} />
+      <Github color={accent} />
     </>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,12 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
+    colors: {
+      dark: '#1F4172',
+      pink: '#F1B4BB',
+      light: '#FDF0F0',
+      darker: '#132043',
+    },
   },
   plugins: [],
 };
