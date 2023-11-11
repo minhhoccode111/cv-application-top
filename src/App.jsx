d71de06f0f522b22ce31ecdfc9ec360c757fec3c
Đặng Hoàng Minh <minhhoccode111@gmail.com>
2023-11-11 19:19:44 +0700

    toggle between personal, education, experience sections

--- src/App.jsx
+++ src/App.jsx
@@ -9,12 +9,10 @@ import 'animate.css';
 import { Input } from './components/Input';
 import { Button } from './components/ToggleButton';
 
-// const FactoryEducation = () => {};
-
 const Display = ({ name, email, phone, address }) => {
   return (
     <div className="my-4 mx-auto max-w-4xl shadow-lg">
-      <header className={'px-4 bg-dark text-pink' + ' ' + (name === '' ? 'py-11' : 'py-6')}>
+      <header className={'px-4 bg-darker text-pink' + ' ' + (name === '' ? 'py-11' : 'py-6')}>
         <h1 className="text-center text-4xl">{name}</h1>
         <h2 className="py-2 flex items-center justify-center gap-2 text-xl">
           {email !== '' ? <Icon.Email color={'#F1B4BB'} height={'24px'} width={'24px'} /> : null}
@@ -29,13 +27,13 @@ const Display = ({ name, email, phone, address }) => {
           {address}
         </h2>
       </header>
+      <div className={'p-4'}>khong dieu kien</div>
     </div>
   );
 };
 
-const Personal = ({ name, email, phone, address, handleChange }) => {
-  const [open, setOpen] = useState(true);
-  const handleOpen = () => setOpen(!open);
+const Personal = ({ name, email, phone, address, handleChange, currentOpen, handleToggle }) => {
+  const isOpen = currentOpen === 'personal';
   return (
     <>
       <div className="p-4 shadow-md shadow-dark max-w-md mx-auto my-4 text-darker relative">
@@ -43,13 +41,51 @@ const Personal = ({ name, email, phone, address, handleChange }) => {
           <Icon.Personal height={'34px'} width={'34px'} /> Personal
         </h1>
         <div className={'absolute right-0 top-0'}>
-          <Button open={open} handleChange={handleOpen} />
+          <Button isOpen={isOpen} handleChange={() => handleToggle('personal')} />
         </div>
-        <div className={open ? ' block' : ' hidden'}>
+        <div className={'' + ' ' + (isOpen ? 'block' : ' hidden')}>
           <Input required={true} placeholder={'First and last name'} infoType={'name'} label={'Full name'} inputType={'text'} value={name} handleChange={handleChange} />
           <Input required={true} placeholder={'Enter your email'} infoType={'email'} label={'Email'} inputType={'email'} value={email} handleChange={handleChange} />
           <Input required={true} placeholder={'Enter phone number'} infoType={'phone'} label={'Phone number'} inputType={'tel'} value={phone} handleChange={handleChange} />
           <Input required={true} placeholder={'City, Country'} infoType={'address'} label={'Address'} inputType={'text'} value={address} handleChange={handleChange} />
+        </div>
+      </div>
+    </>
+  );
+};
+
+const Education = ({ school, degree, startDate, endDate, location, handleChange, currentOpen, handleToggle }) => {
+  const isOpen = currentOpen === 'education';
+  return (
+    <>
+      <div className="p-4 shadow-md shadow-dark max-w-md mx-auto my-4 text-darker relative">
+        <h1 className="flex items-center gap-2 text-3xl font-bold bg-white">
+          <Icon.Education height={'34px'} width={'34px'} /> Education
+        </h1>
+        <div className={'absolute right-0 top-0'}>
+          <Button isOpen={isOpen} handleChange={() => handleToggle('education')} />
+        </div>
+        <div className={'' + ' ' + (isOpen ? 'block' : ' hidden')}>
+          <div className="bg-light h-40"></div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+const Experience = ({ name, email, phone, address, handleChange, currentOpen, handleToggle }) => {
+  const isOpen = currentOpen === 'experience';
+  return (
+    <>
+      <div className="p-4 shadow-md shadow-dark max-w-md mx-auto my-4 text-darker relative">
+        <h1 className="flex items-center gap-2 text-3xl font-bold bg-white">
+          <Icon.Experience height={'34px'} width={'34px'} /> Experience
+        </h1>
+        <div className={'absolute right-0 top-0'}>
+          <Button isOpen={isOpen} handleChange={() => handleToggle('experience')} />
+        </div>
+        <div className={'' + ' ' + (isOpen ? 'block' : ' hidden')}>
+          <div className="bg-light h-40"></div>
         </div>
       </div>
     </>
@@ -63,11 +99,22 @@ export const App = () => {
     phone: '+84 123123123',
     address: 'District 10, Ho Chi Minh city, Viet Nam',
   });
-  const handleInfoChange = (type, value) => setInfo({ ...personalInfo, [type]: value });
+  const handlePersonalInfoChange = (type, value) => setInfo({ ...personalInfo, [type]: value });
+
+  const [currentOpen, setCurrentOpen] = useState('personal');
+  const handleCurrentOpenChange = (newCurrentOpen) => {
+    if (newCurrentOpen === currentOpen) {
+      setCurrentOpen(null);
+    } else {
+      setCurrentOpen(newCurrentOpen);
+    }
+  };
 
   return (
     <div id="wrapper" className="min-h-screen">
-      <Personal {...personalInfo} handleChange={handleInfoChange} />
+      <Personal {...personalInfo} handleChange={handlePersonalInfoChange} currentOpen={currentOpen} handleToggle={handleCurrentOpenChange} />
+      <Education currentOpen={currentOpen} handleToggle={handleCurrentOpenChange} />
+      <Experience currentOpen={currentOpen} handleToggle={handleCurrentOpenChange} />
       <Display {...personalInfo} />
     </div>
   );
--- src/components/ToggleButton.jsx
+++ src/components/ToggleButton.jsx
@@ -1,10 +1,10 @@
 /* eslint-disable react/prop-types */
 import { Up, Down } from './Icons';
 
-export const Button = ({ handleChange, open }) => {
+export const Button = ({ handleChange, isOpen }) => {
   return (
     <button type="button" onClick={handleChange} className={'p-4 mx-4 my-1'}>
-      {open ? <Up color="#132043" height={'30px'} width={'30px'} /> : <Down color="#132043" height={'30px'} width={'30px'} />}
+      {isOpen ? <Up color="#132043" height={'30px'} width={'30px'} /> : <Down color="#132043" height={'30px'} width={'30px'} />}
     </button>
   );
 };
